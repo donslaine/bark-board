3188d8de1b50667f17e8a8e9986a2e860c9eb699
Paul Truitt <paultruitt@Pauls-MacBook-Pro.local>
2023-02-17 19:14:48 -0500

    adds delete functionality Co-authored-by: Patrick pat.mangan@outlook.com quote> Co-authored-by: Erika erikadphelm@gmail.com quote> Co-authored-by: Lewis Lewisg735@gmail.com quote> Co-authored-by: Heather hknapp15@su.edu

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,12 +1,12 @@
 
-export default function Post ({ post }) {
-    console.log(post)
+export default function Post ({ post, deletePost }) {
     return (
-    <div>
-        <h2>{post.title}</h2>
-        <p>{post.pet}</p>
-        <p>{post.text}</p>
-        <p>{post.category}</p>
-    </div>
+        <div>
+            <h2>{post.title}</h2>
+            <p>{post.pet}</p>
+            <p>{post.text}</p>
+            <p>{post.category}</p>
+            <button onClick={() => deletePost(post._id)}>Delete</button>
+        </div>
     )
 }
--- src/pages/GlobalPage/GlobalPage.js
+++ src/pages/GlobalPage/GlobalPage.js
@@ -1,30 +1,28 @@
 import { useEffect, useState } from 'react'
 import CreatePost from '../../components/CreatePost/CreatePost'
-import { index } from '../../utilities/posts-api'
+import { index, removePost } from '../../utilities/posts-api'
 import Post from '../../components/Post/Post'
 
 export default function GlobalPage() {
     const [postArr, setPostArr] = useState([])
 
-
-
     useEffect(() => {
         index()
             .then((res)=> res.json())
-            // .then((resData) => console.log(resData))
-            .then((resData) => setPostArr(resData.posts))
-            // .then(console.log(postArr))
-            
-    }, []) 
+            .then((resData) => setPostArr(resData.posts))           
+    }, [postArr])
+
+    function deletePost(id) {
+        removePost(id)
+    }
+
     const postMap = postArr.map((post, index) => (
-        <Post post={post} key={index} />
+        <Post post={post} key={index} deletePost={deletePost} />
     ))
-    
     
     return (
         <div>
             <h2>GlobalPage</h2>
-            
             <CreatePost />
             {postMap}
         </div>
