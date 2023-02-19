e1972235f50587c180628ebeb6a368216aa9d1ae
Paul Truitt <paultruitt@Pauls-MacBook-Pro.local>
2023-02-18 20:14:36 -0500

    fixes comment delete bug

--- controllers/api/comments.js
+++ controllers/api/comments.js
@@ -3,6 +3,7 @@ const Comment = require("../../models/comment")
 
 function createComment(req, res, next) {
   const comment = req.body.comments
+  // comment.owner = req.user._id
   const postId = req.body.comments.postId
   Post.findById(postId)
     .then((post) =>{
@@ -15,6 +16,24 @@ function createComment(req, res, next) {
     .catch(next)
 }
 
+function deleteComment(req, res, next) {
+  const commentId = req.params.commentId
+  const postId = req.body.comments.postId
+  Post.findById(postId)
+    .then(post => {
+      post.comments.id(commentId).remove()
+      // post.comments.forEach(index => {
+      //   if (index.id === commentId) {
+      //     index.remove()
+      //   }
+      // })
+      return post.save()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+}
+
 module.exports = {
   createComment,
+  deleteComment
 }
--- routes/api/comments.js
+++ routes/api/comments.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const commentsCtrl = require("../../controllers/api/comments")
 
 router.post("/new", commentsCtrl.createComment)
+router.delete("/:commentId", commentsCtrl.deleteComment)
 
 module.exports = router
