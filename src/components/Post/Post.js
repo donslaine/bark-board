10935ada44226c0a4f65b7380f3b2294d6acb1e9
Patrick Mangan <patrickmangan@Patricks-Mini.lan>
2023-02-20 12:36:32 -0500

    basic ui touch up Co-authored:Erika Helm erikadphelm@gmail.com Heather Knapp hknapp15@su.edu Patrick Mangan pat.mangan@outlook.com Paul Truitt truittpaul39@gmail.com Lewis Graves Lewisg735@gmail.com

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,34 +1,35 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import ShowComment from '../ShowComment/ShowComment'
-import {createComment, deleteComment} from "../../utilities/comments-api.js"
-export default function Post ({ post, deletePost }) {
+import { createComment, deleteComment } from "../../utilities/comments-api.js"
+export default function Post({ post, deletePost }) {
 
-    const[comment, setComment] = useState({
+    const [comment, setComment] = useState({
         text: "",
         postId: `${post._id}`
     })
 
     function onCommentChange(event) {
-        setComment({...comment, 
-        [event.target.name]: event.target.value,
-        postId: `${post._id}`
+        setComment({
+            ...comment,
+            [event.target.name]: event.target.value,
+            postId: `${post._id}`
         })
     }
 
     async function onSubmit(event) {
         event.preventDefault()
         try {
-            const formData = {...comment}
+            const formData = { ...comment }
             console.log(formData)
             await createComment(formData)
-        } catch(error) {
+        } catch (error) {
             console.error(error)
         }
     }
 
     function handleDeleteComment(event) {
-        const deleteReq = {comments: { postId: post._id }}
+        const deleteReq = { comments: { postId: post._id } }
         deleteComment(deleteReq, event.target.id)
     }
 
@@ -40,29 +41,31 @@ export default function Post ({ post, deletePost }) {
                 <p>{post.text}</p>
                 <p>{post.category}</p>
                 <button className='btn btn-danger mx-2 my-2' onClick={() => deletePost(post._id)}>Delete</button>
-            <Link to={`/posts/${post._id}`} >
-                <button 
-                    className='btn btn-primary mx-2 my-2'
-                >Update</button>
-            </Link>
+                <Link to={`/posts/${post._id}`} >
+                    <button
+                        className='btn btn-primary mx-2 my-2'
+                    >Update</button>
+                </Link>
 
-            <label>Create Comment</label>
-            <input 
-                type="text" 
-                name="text" 
-                onChange={onCommentChange}
-            />
-            <button 
-                type="submit"
-                onClick={onSubmit}
-            >Create</button>
-        
-            <ShowComment 
-                postComment={post.comments} 
+                <div>
+                    <label>Create Comment</label>
+                    <input
+                        type="text"
+                        name="text"
+                        onChange={onCommentChange}
+                    />
+                    <button
+                        type="submit"
+                        onClick={onSubmit}
+                    >Create</button>
+                </div>
+            </div>
+            <ShowComment
+                postComment={post.comments}
                 handleDeleteComment={handleDeleteComment}
             />
 
-            </div>
+
         </>
     )
 }
--- src/components/ShowComment/CommentBody.js
+++ src/components/ShowComment/CommentBody.js
@@ -2,9 +2,13 @@ export default function CommentBody({ text, handleDeleteComment, id }) {
 
 
     return (
-        <>
+        <div className="border rounded-2 my-2">
+        <div className="col my-1">
         {text}
-        <button onClick={handleDeleteComment} id={id}>Delete Comment</button>
-        </>
+        </div>
+        <div className="col my-1">
+        <button className="btn btn-danger" onClick={handleDeleteComment} id={id}>-</button>
+        </div>
+        </div>
     )
 }
