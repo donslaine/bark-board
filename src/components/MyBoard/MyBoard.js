b52df07af82b608d8273e825ad12e5b71ab5c644
erika helm <erikadphelm16@gmail.com>
2023-02-21 08:21:20 -0600

    Getting MyBoard page working on front end

---
+++ src/components/MyBoard/MyBoard.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react"
+import { indexBoard } from "../../utilities/posts-api";
+export default function MyBoard( postsSorted ) {
+    const posts = [postsSorted]
+console.log(posts)
+
+
+    return (
+    <h2>myboard</h2>
+    )
+}
