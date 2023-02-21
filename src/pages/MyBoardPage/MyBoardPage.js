d7896805819f70cd7d156a7adff904e628efdb89
erika helm <erikadphelm16@gmail.com>
2023-02-21 08:25:11 -0600

    Getting MyBoard page working on front end

--- src/pages/MyBoardPage/MyBoardPage.js
+++ src/pages/MyBoardPage/MyBoardPage.js
@@ -6,33 +6,38 @@ let sortedPost = []
 const postsSorted = sortedPost
     const [showBoard, setShowBoard] = useState({})
     console.log(sortedPost)
-        useEffect(() => {
-            indexBoard()
-                .then((res)=> res.json())
-                .then((resData) => setShowBoard(resData.post))   
-        },[])
+        // useEffect(() => {
+        //     indexBoard()
+        //         .then((res)=> res.json())
+        //         .then((resData) => setShowBoard(resData.post))   
+        // },[])
     
-        
-
-          
+   
+     
+            setPostsVisible(!postsVisible)
             postList.map((post, index)=>{ 
         
        
-           
-                    <MyBoard post={post} key={index}/>
-                    sortedPost.push(post)
-                 
+        //         if (post.owner._id === user._id){
+        //             <MyBoard post={post} key={index}/>
+        //             sortedPost.push(post)
+        //             return sortedPost
                 
               
-                }   
+        //         }   
             
-            )}
-       
+            })
+        
 
+indexBoard()
+    
+    function showPosts() {
 
+    }
+    
     
     return (
         <h2>{<MyBoard/>}</h2>
         
     )
- 
+}
